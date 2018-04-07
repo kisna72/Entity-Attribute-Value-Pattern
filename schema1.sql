c59d013a9adbdf8ae072e6aaeaabc9f61fdac22d
Krishna Regmi <krishna.regmi@intralox.com>
2018-04-07 11:44:41 -0500

    add code  to add  values to the tables

--- schema1.sql
+++ schema1.sql
@@ -1,18 +1,34 @@
 -- Create  Entity Type  Table.
 -- Used to create New Custom  Table essentially.
+-- Two Examples are  Project  and  product
 DROP TABLE IF EXISTS entity_type CASCADE;
 CREATE TABLE entity_type
-( id SERIAL PRIMARY KEY,
-  name text NOT NULL,
+( 
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL
 );
+-- Add Values to the Table.
+INSERT INTO entity_type (name) VALUES ( 'Product' );
+INSERT INTO entity_type (name) VALUES ( 'Project' );
+-- Get all  the added values for testing purpose
+SELECT * FROM entity_type;
 
+-- Add Rows here for the Entity Type that was created. 
+-- Multiple Projects or multiple products. Makes the code generic.
 DROP TABLE IF EXISTS entity CASCADE;
 CREATE TABLE entity
 (
     id SERIAL PRIMARY KEY,
-    constraint entity_type_pk PRIMARY KEY (entity_type),
+    entity_type_pk integer REFERENCES entity_type (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    label text NOT NULL
 );
+-- Add  Values to the Table for testing
+INSERT INTO entity (entity_type_pk, label) VALUES (2, 'Iphone  Camera Design Project');
+INSERT INTO entity (entity_type_pk, label) VALUES (2, 'MacBok Costdown Project');
+-- Print
+SELECT * FROM entity;
 
+-- Create  Table to add the attribute for each entity type.
 DROP TABLE IF EXISTS attribute CASCADE;
 CREATE TABLE attribute 
 (
