e6757eac6566ff7ead287ba8544ca7b789706e3f
Krishna Regmi <krishna.regmi@intralox.com>
2018-04-07 12:21:40 -0500

    code to  add attribute and attribute value

--- schema1.sql
+++ schema1.sql
@@ -33,14 +33,25 @@ DROP TABLE IF EXISTS attribute CASCADE;
 CREATE TABLE attribute 
 (
     id SERIAL PRIMARY KEY,
-    label TEXT NOT NULL,
+    entity_type INTEGER REFERENCES entity_type (id) on DELETE CASCADE On UPDATE  CASCADE,
+    name TEXT NOT NULL
 );
+-- Simulating User adding columns to their tables. (lets say for project table)
+INSERT INTO  attribute (entity_type, name) VALUES (2, 'Project Engineer');
+INSERT INTO  attribute (entity_type, name) VALUES (2, 'Project Manager' );
 
+select * from attribute;
+
+-- Create attribute_value table. attribute value is added for each.
 DROP TABLE IF EXISTS attribute_value CASCADE;
 CREATE TABLE attribute_value
 (
     id SERIAL PRIMARY KEY,
-    CONSTRAINT attribute_id PRIMARY KEY (attribute),
-    CONSTRAINT entity_id PRIMARY KEY (entity),
+    attribute_id INTEGER REFERENCES attribute (id) on DELETE CASCADE On UPDATE  CASCADE,
+    entity_id INTEGER REFERENCES entity (id) on DELETE CASCADE On UPDATE  CASCADE,
     value TEXT NULL
 );
+
+INSERT INTO attribute_value (attribute_id, entity_id, value) VALUES (1,1,'John Smith');
+INSERT INTO attribute_value (attribute_id, entity_id, value) VALUES (1,2,'Jacques Moser');
+INSERT INTO attribute_value (attribute_id, entity_id, value) VALUES (2,1,'John Doe');
