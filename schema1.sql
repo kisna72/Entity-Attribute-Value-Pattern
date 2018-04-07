63d0e9f53da59f80ffad94e85684e5a0357cd342
Krishna Regmi <Krishna.Regmi@Intralox.com>
2018-04-07 14:40:11 -0500

    add some tables and sql out

--- schema1.sql
+++ schema1.sql
@@ -24,9 +24,10 @@ CREATE TABLE entity
 );
 -- Add  Values to the Table for testing
 INSERT INTO entity (entity_type_pk, label) VALUES (2, 'Iphone  Camera Design Project');
-INSERT INTO entity (entity_type_pk, label) VALUES (2, 'MacBok Costdown Project');
+INSERT INTO entity (entity_type_pk, label) VALUES (2, 'MacBook Costdown Project');
 -- Print
 SELECT * FROM entity;
+
 
 -- Create  Table to add the attribute for each entity type.
 DROP TABLE IF EXISTS attribute CASCADE;
@@ -41,6 +42,9 @@ INSERT INTO  attribute (entity_type, name) VALUES (2, 'Project Engineer');
 INSERT INTO  attribute (entity_type, name) VALUES (2, 'Project Manager' );
 
 select * from attribute;
+/*
+
+*/
 
 -- Create attribute_value table. attribute value is added for each.
 DROP TABLE IF EXISTS attribute_value CASCADE;
@@ -55,3 +59,63 @@ CREATE TABLE attribute_value
 INSERT INTO attribute_value (attribute_id, entity_id, value) VALUES (1,1,'John Smith');
 INSERT INTO attribute_value (attribute_id, entity_id, value) VALUES (1,2,'Jacques Moser');
 INSERT INTO attribute_value (attribute_id, entity_id, value) VALUES (2,1,'John Doe');
+
+
+
+/*
+
+Entity
++----+------------------------------+-------------+--+--+
+| id | label                        | Entity Type |  |  |
++----+------------------------------+-------------+--+--+
+| 1  | Iphone Camera Design project | 1 (project) |  |  |
++----+------------------------------+-------------+--+--+
+| 2  | Mac book CostDown            | 1 (project) |  |  |
++----+------------------------------+-------------+--+--+
+|  3 | Test                         | 1 (project) |  |  |
++----+------------------------------+-------------+--+--+
+
+
+Attribute
++----+-------------+------------------+--+--+
+| id | Entity Type | Name             |  |  |
++----+-------------+------------------+--+--+
+| 1  | 1           | Project Engineer |  |  |
++----+-------------+------------------+--+--+
+| 2  | 1           | Project  Manager |  |  |
++----+-------------+------------------+--+--+
+| 3  | 1           | Project Cost     |  |  |
++----+-------------+------------------+--+--+
+
+
+Attribute Value
++----+--------------+-----------+-------------+--+
+| id | attribute id | entity id | value       |  |
++----+--------------+-----------+-------------+--+
+| 1  | 2            | 1         | John Doe    |  |
++----+--------------+-----------+-------------+--+
+| 2  | 2            | 1         | John Smith  |  |
++----+--------------+-----------+-------------+--+
+| 3  | 2            | 1         | John Carry  |  |
++----+--------------+-----------+-------------+--+
+
+
+We want to get...
+
++----+------------------------------+-------------+-------------------+------------------+---------------+
+| id | label                        | entity type | Project Enginener | Project  Manager | Project Cost  |
++----+------------------------------+-------------+-------------------+------------------+---------------+
+| 1  | Iphone Camera Design project | 1           | John Doe          | John Smith       | john Carry    |
++----+------------------------------+-------------+-------------------+------------------+---------------+
+| 2  | Mac book CostDown            | 1           |                   |                  |
++----+------------------------------+-------------+-------------------+------------------+
+| 3  | test                         | 1           |                   |                  |
++----+------------------------------+-------------+-------------------+------------------+
+
+*/
+
+
+-- How to  make queries? 
+-- 1. Filter entity by the entity type you want. 
+-- 2. Get attributes from this entity type. and turn them into columns.
+-- 3. For each column, get the value from the Values table, and use that to fill  in for each row.
